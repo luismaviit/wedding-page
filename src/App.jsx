d1d7828a490ca1ebd17dbf9a13d56e1a1ed613a5
luismaviit <69017415+luismaviit@users.noreply.github.com>
2024-08-20 15:50:08 -0500

    add button inicar ruta

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import "./App.css";
 
 function App() {
+  const openGoogleMaps = () => {
+    const destination = "11.0400684,-74.9122979"; // Coordenadas de destino (latitud, longitud)
+    const url = `https://www.google.com/maps/dir/?api=1&destination=${destination}`;
+    window.open(url, "_blank");
+  };
   return (
     <>
       <div className="hero">
@@ -23,15 +28,19 @@ function App() {
       {/* Sección del Mapa debajo del Hero */}
       <div className="map-section">
         <iframe
-        className="map-iframe"
+          className="map-iframe"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3915.980770841985!2d-74.9122979!3d11.040068399999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8ef42bd1ca7a3b29%3A0xa39b67f814f3e3eb!2sCasa%20Campestre%20El%20LOTE!5e0!3m2!1ses!2sco!4v1724185873456!5m2!1ses!2sco"
           width="100%"
-          height="600px"
           style={{ border: 3 }}
           allowFullScreen=""
           loading="lazy"
           referrerPolicy="no-referrer-when-downgrade"
         ></iframe>
+        <div className="button-section">
+          <button onClick={openGoogleMaps} className="route-button">
+            Iniciar Ruta
+          </button>
+        </div>
       </div>
     </>
   );
