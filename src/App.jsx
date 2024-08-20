c68defc470bfef0d1e9e532452b7fdbd5d383274
luismaviit <69017415+luismaviit@users.noreply.github.com>
2024-08-20 14:29:18 -0500

    add media query

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,6 @@ function App() {
               Keren & <br />
               Ruben
             </h1>
-            <div className="two-section">
-              <h3 className="date"> Noviembre</h3>
-              <h1 className="day-wedding">16</h1>
-              <h3 className="date"> 2024</h3>
-            </div>
           </div>
         </div>
       </div>
