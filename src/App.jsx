95aa6f9b3cbb711cef96e948d09829d84e72b4a8
luismaviit <69017415+luismaviit@users.noreply.github.com>
2024-08-20 16:51:55 -0500

    cambio de fecha y orden de nombre

--- src/App.jsx
+++ src/App.jsx
@@ -13,13 +13,13 @@ function App() {
           <div className="tittles-hero">
             <span className="sub-tittle"> La boda de ...</span>
             <h1 className="tittle-hero">
-              Keren & <br />
-              Ruben
+              Ruben & <br />
+              Keren
             </h1>
           </div>
           <div className="two-section">
-            <h3 className="date"> Noviembre</h3>
-            <h1 className="day-wedding">16</h1>
+            <h3 className="date"> 16</h3>
+            <h1 className="day-wedding"> Nov</h1>
             <h3 className="date"> 2024</h3>
           </div>
         </div>
