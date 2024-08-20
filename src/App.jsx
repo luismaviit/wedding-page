2aedd0cd8c0a85057baa7e87a0a86f1714a34649
luismaviit <69017415+luismaviit@users.noreply.github.com>
2024-08-20 14:02:03 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div class="hero">
+        <div class="hero-content">
+          <div className="tittles-hero">
+            <span className="sub-tittle"> La boda de ...</span>
+            <h1 className="tittle-hero">
+              Keren & <br />
+              Ruben
+            </h1>
+            <div className="two-section">
+              <h3 className="date"> Noviembre</h3>
+              <h1 className="day-wedding">16</h1>
+              <h3 className="date"> 2024</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
