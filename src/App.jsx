766c05c090c0b8f1f67e34545a04dd0cf921fc7e
luismaviit <69017415+luismaviit@users.noreply.github.com>
2024-08-21 10:01:01 -0500

    se agrego nav con ruta y scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Link } from "react-scroll";
 
 function App() {
   const openGoogleMaps = () => {
@@ -9,6 +10,34 @@ function App() {
   return (
     <>
       <div className="hero">
+        <nav className="nav">
+          <Link
+            activeClass="active"
+            to="route"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
+            <span className="span-button">
+              como llegar <i className="bi bi-geo-alt-fill"></i>
+            </span>
+          </Link>
+          <Link
+            activeClass="active"
+            to="route"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
+            {" "}
+            <span className="span-button">
+              confirmar asistencia
+              <i className="bi bi-check-circle-fill"></i>
+            </span>
+          </Link>
+        </nav>
         <div className="hero-content">
           <div className="tittles-hero">
             <span className="sub-tittle"> La boda de ...</span>
@@ -27,10 +56,14 @@ function App() {
         <div className="row row-biblical-quotation">
           <div className="col biblical-quotation">
             <h3 className="biblical-text">
-              El amor es sufrido, es benigno; el amor no tiene envidia, el amor <br/>
-              no es jactancioso, no se envanece; no hace nada indebido, no busca<br/>
-              lo suyo, no se irrita, no guarda rencor; no se goza de la<br/>
-              injusticia, mas se goza de la verdad. Todo lo sufre, todo lo cree,<br/>
+              El amor es sufrido, es benigno; el amor no tiene envidia, el amor{" "}
+              <br />
+              no es jactancioso, no se envanece; no hace nada indebido, no busca
+              <br />
+              lo suyo, no se irrita, no guarda rencor; no se goza de la
+              <br />
+              injusticia, mas se goza de la verdad. Todo lo sufre, todo lo cree,
+              <br />
               todo lo espera, todo lo soporta.
             </h3>
             <h1>Corintios 13:4-8</h1>
@@ -47,7 +80,7 @@ function App() {
       </div>
 
       {/* Sección del Mapa debajo del Hero */}
-      <div className="map-section">
+      <div id="route" className="map-section">
         <iframe
           className="map-iframe"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3915.980770841985!2d-74.9122979!3d11.040068399999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8ef42bd1ca7a3b29%3A0xa39b67f814f3e3eb!2sCasa%20Campestre%20El%20LOTE!5e0!3m2!1ses!2sco!4v1724185873456!5m2!1ses!2sco"
